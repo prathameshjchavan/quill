e1688d0abc1f7e9e83fbc86307ef0cdb1297647c
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-09 11:35:25 +0530

    added landing page

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 type Props = {
@@ -7,7 +8,12 @@ type Props = {
 
 const MaxWidthWrapper = ({ className, children }: Props) => {
 	return (
-		<div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+		<div
+			className={cn(
+				"mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+				className
+			)}
+		>
 			{children}
 		</div>
 	);
