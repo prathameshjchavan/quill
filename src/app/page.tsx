7524e79243e4506f29ec980f71902566beaa2513
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-09 11:49:22 +0530

    styled link as button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
 	return (
@@ -19,7 +20,14 @@ export default function Home() {
 				upload your file and start asking questions right away.
 			</p>
 
-			<Link href="/" target="_blank" className="flex">
+			<Link
+				href="/"
+				target="_blank"
+				className={buttonVariants({
+					size: "lg",
+					className: "mt-5",
+				})}
+			>
 				Get Started <ArrowRight className="ml-2 h-5 w-5" />
 			</Link>
 		</MaxWidthWrapper>
