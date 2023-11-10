41c46a6611ea63443f31d9a84aaea15c951fa6b3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-10 17:58:10 +0530

    added loading skeleton

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,6 +3,7 @@
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
 import { GhostIcon } from "lucide-react";
+import Skeleton from "react-loading-skeleton";
 
 const Dashboard = () => {
 	const { data: files, isLoading } = trpc.getUserFiles.useQuery();
@@ -19,7 +20,7 @@ const Dashboard = () => {
 			{files && !!files.length ? (
 				<div></div>
 			) : isLoading ? (
-				<div></div>
+				<Skeleton height={100} className="my-2" count={3} />
 			) : (
 				<div className="mt-16 flex flex-col items-center gap-2">
 					<GhostIcon className="h-8 w-8 text-zinc-800" />
