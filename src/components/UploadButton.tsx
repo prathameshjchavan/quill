8fae5c4f853c7f61915dfcd02ad053cc559be3c6
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-10 17:22:14 +0530

    added upload button

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
 
 const UploadButton = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 
-	return <div></div>;
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogTrigger onClick={() => setIsOpen(true)} asChild>
+				<Button>Upload PDF</Button>
+			</DialogTrigger>
+			<DialogContent>Hello</DialogContent>
+		</Dialog>
+	);
 };
 
 export default UploadButton;
