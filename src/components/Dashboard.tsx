09c8b14d32fb9b394eb2d41e20e39fa42e4d92fe
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-10 17:48:56 +0530

    added empty files UI

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
+import { GhostIcon } from "lucide-react";
 
 const Dashboard = () => {
+	const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
 	return (
 		<main className="mx-auto max-w-7xl md:p-10">
 			<div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
@@ -10,6 +16,17 @@ const Dashboard = () => {
 			</div>
 
 			{/* display all user files */}
+			{files && !!files.length ? (
+				<div></div>
+			) : isLoading ? (
+				<div></div>
+			) : (
+				<div className="mt-16 flex flex-col items-center gap-2">
+					<GhostIcon className="h-8 w-8 text-zinc-800" />
+					<h3 className="font-semibold text-xl">Pretty empty around here</h3>
+					<p>Let&apos;s upload your first PDF.</p>
+				</div>
+			)}
 		</main>
 	);
 };
