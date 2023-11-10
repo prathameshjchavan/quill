4dc12fd5f5a977f5928f0ed44751f058e3ec9403
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-10 17:16:55 +0530

    added dashboard

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+const UploadButton = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	return <div></div>;
+};
+
+export default UploadButton;
