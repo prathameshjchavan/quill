700f33f99a96374c6df0324e1f79b0188f2ce43b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-11 18:45:15 +0530

    implemented zoom functionality

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
+import {
+	ChevronDown,
+	ChevronDownIcon,
+	ChevronUp,
+	Loader2,
+	SearchIcon,
+} from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
@@ -14,6 +20,13 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import SimpleBar from "simplebar-react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -25,6 +38,7 @@ const PdfRenderer = ({ url }: Props) => {
 	const { toast } = useToast();
 	const [numPages, setNumPages] = useState<number>();
 	const [currPage, setCurrPage] = useState<number>(1);
+	const [scale, setScale] = useState<number>(1);
 
 	const CustomPageValidator = z.object({
 		page: z.string().refine((num) => {
@@ -98,32 +112,60 @@ const PdfRenderer = ({ url }: Props) => {
 						<ChevronUp className="h-4 w-4" />
 					</Button>
 				</div>
+
+				<div className="space-x-2">
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button className="gap-1.5" aria-label="zoom" variant="ghost">
+								<SearchIcon className="h-4 w-4" />
+								{scale * 100}%
+								<ChevronDownIcon className="h-3 w-3 opacity-50" />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<DropdownMenuItem onSelect={() => setScale(1)}>
+								100%
+							</DropdownMenuItem>
+							<DropdownMenuItem onSelect={() => setScale(1.5)}>
+								150%
+							</DropdownMenuItem>
+							<DropdownMenuItem onSelect={() => setScale(2)}>
+								200%
+							</DropdownMenuItem>
+							<DropdownMenuItem onSelect={() => setScale(2.5)}>
+								250%
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
 			</div>
 
 			<div className="flex-1 w-full max-h-screen">
-				<div ref={ref}>
-					<Document
-						loading={
-							<div className="flex justify-center">
-								<Loader2 className="my-24 h-6 w-6 animate-spin" />
-							</div>
-						}
-						onLoadError={() => {
-							toast({
-								title: "Error loading PDF",
-								description: "Please try again later",
-								variant: "destructive",
-							});
-						}}
-						onLoadSuccess={({ numPages }) => {
-							setNumPages(numPages);
-						}}
-						file={url}
-						className="max-h-full"
-					>
-						<Page width={width || 1} pageNumber={currPage} />
-					</Document>
-				</div>
+				<SimpleBar autoHide={false} className="max-h-[calc(100vh - 10rem)]">
+					<div ref={ref}>
+						<Document
+							loading={
+								<div className="flex justify-center">
+									<Loader2 className="my-24 h-6 w-6 animate-spin" />
+								</div>
+							}
+							onLoadError={() => {
+								toast({
+									title: "Error loading PDF",
+									description: "Please try again later",
+									variant: "destructive",
+								});
+							}}
+							onLoadSuccess={({ numPages }) => {
+								setNumPages(numPages);
+							}}
+							file={url}
+							className="max-h-full"
+						>
+							<Page width={width || 1} scale={scale} pageNumber={currPage} />
+						</Document>
+					</div>
+				</SimpleBar>
 			</div>
 		</div>
 	);
