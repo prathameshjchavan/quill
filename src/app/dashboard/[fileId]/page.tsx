2f1f2852a778da7358996217035ec958cd3e29e1
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-11 10:00:08 +0530

    file page top level layout

---
+++ src/app/dashboard/[fileId]/page.tsx
@@ -0,0 +1,44 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+interface Props {
+	params: {
+		fileId: string;
+	};
+}
+
+const Page = async ({ params: { fileId } }: Props) => {
+	const { getUser } = getKindeServerSession();
+	const user = await getUser();
+
+	if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileId}`);
+
+	const file = await db.file.findFirst({
+		where: { id: fileId, userId: user.id },
+	});
+
+	if (!file) notFound();
+
+	return (
+		<div className="flex-1 justify-between flex-col h-[calc(100vh-3.5rem)]">
+			<div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+				{/* Left Side */}
+				<div className="flex-1 xl:flex">
+					<div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+						<PdfRenderer />
+					</div>
+				</div>
+
+				{/* Right Side */}
+				<div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+					<ChatWrapper />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Page;
