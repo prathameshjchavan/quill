31092e516db9a1f80c6d4d7b34df74a3e2e5d948
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-11 10:45:05 +0530

    increment progress bar logic

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -8,13 +8,36 @@ import { CloudIcon, FileIcon } from "lucide-react";
 import { Progress } from "./ui/progress";
 
 const UploadDropzone = () => {
-	const [isUploading, setIsUploading] = useState<boolean | null>(true);
+	const [isUploading, setIsUploading] = useState<boolean | null>(false);
+	const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+	const startSimulatedProgress = () => {
+		setUploadProgress(0);
+
+		const interval = setInterval(() => {
+			setUploadProgress((prevProgress) => {
+				if (prevProgress >= 95) {
+					clearInterval(interval);
+					return prevProgress;
+				}
+				return prevProgress + 5;
+			});
+		}, 500);
+
+		return interval;
+	};
 
 	return (
 		<Dropzone
 			multiple={false}
 			onDrop={(acceptedFile) => {
-				console.log(acceptedFile);
+				setIsUploading(true);
+
+				const progressInterval = startSimulatedProgress();
+
+				// handle file uploading
+				clearInterval(progressInterval);
+				setUploadProgress(100);
 			}}
 		>
 			{({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -49,7 +72,10 @@ const UploadDropzone = () => {
 
 							{isUploading ? (
 								<div className="w-full mt-4 max-w-xs mx-auto">
-									<Progress value={50} className="h-1 w-full bg-zinc-200" />
+									<Progress
+										value={uploadProgress}
+										className="h-1 w-full bg-zinc-200"
+									/>
 								</div>
 							) : null}
 						</label>
