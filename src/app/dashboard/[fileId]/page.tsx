4a271069336c7ce95ed5bf34a5f0b826ae3f7adb
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-11 14:04:44 +0530

    rendering pdf

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -28,7 +28,7 @@ const Page = async ({ params: { fileId } }: Props) => {
 				{/* Left Side */}
 				<div className="flex-1 xl:flex">
 					<div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-						<PdfRenderer />
+						<PdfRenderer url={file.url} />
 					</div>
 				</div>
 
