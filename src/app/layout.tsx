a75808795505e5515f222349c6a033e48ad5b545
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-11 12:19:17 +0530

    added shadcn toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 
 import "react-loading-skeleton/dist/skeleton.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
 				>
 					<Navbar />
 					{children}
+					<Toaster />
 				</body>
 			</Providers>
 		</html>
