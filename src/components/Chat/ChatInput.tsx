6df2704fcb7486bf9376c84f885ef1fe899a61ba
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-12 14:26:02 +0530

    changed file directory

---
+++ src/components/Chat/ChatInput.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+	isDisabled?: boolean;
+}
+
+const ChatInput = ({ isDisabled }: Props) => {
+	return <div>ChatInput</div>;
+};
+
+export default ChatInput;
